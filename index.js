64e6608f21fc0bd76088937454376f6305e80c2b
joyjitdas3918 <138395405+joyjitdas3918@users.noreply.github.com>
2024-08-29 12:00:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const interval = 30000; // Interval in milliseconds (30 seconds)
 function reloadWebsite() {
   axios.get(url)
     .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
     })
     .catch(error => {
       console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
